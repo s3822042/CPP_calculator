e67d3b62b7350299e44f10de44cb124fa001b837
Luan Vo Thanh <s3822042@rmit.edu.vn>
2021-04-05 18:05:48 +0700

    add if/else logib

--- main.cpp
+++ main.cpp
@@ -7,6 +7,47 @@
 
 using namespace std;
 
+void getUserInput(){
+    int score_asm;
+    cout << "Please enter your score: ";
+    cin >> score_asm;
+}
+
+int calculateScore(){
+    int final_score = 60;
+    return final_score;
+
+}
+
+void printQualification(){
+    int score = calculateScore();
+    if(score >= 50 && score <60){
+        cout << "PA";
+    }
+    else if(score >= 60 && score < 70){
+        cout << "CR";
+    }
+    else if(score >= 70 && score < 80){
+        cout << "DI";
+    }
+    else if(score >= 80){
+        cout << "HD";
+    }
+    else{
+        cout <<"NN";
+    }
+}
+void finalizeScore(){
+    printQualification();
+
+}
+
+void calculationGPA(){
+
+}
+
+
+
 void printMenu(){
     cout << "Welcome to Calculation Program\n";
     cout << "1. Calculate score\n";
@@ -20,17 +61,21 @@ void switchMenu(){
     cin >> userInput;
     switch (userInput) {
         case 1:
+            finalizeScore();
             break;
         case 2:
+            calculationGPA();
             break;
         case 3:
             exit(-1);
-
+        default:
+            break;
     }
 }
-
 int main(){
     printMenu();
     switchMenu();
+
+    return 0;
 }
 
